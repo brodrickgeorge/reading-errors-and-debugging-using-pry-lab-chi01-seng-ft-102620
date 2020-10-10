e9b03252c307e3f00d41b541df614a6875d3c44b
Brodrick George <brodrick.george@yahoo.com>
2020-10-10 01:49:59 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,10 +1,10 @@
-require 'pry' # don't forget to add: require 'pry'
+ # don't forget to add: require 'pry'
 
 def start_game(Rocky, Bullwinkle)
  puts "Hello #{player1} & #{playar2}"
 
  end
- binding.pry
+
 
 def play_game(player2)
  puts "#{player1} is better than #{player2}"
